c60b9806265cd431122b991a23b882e2bce839ee
Kevin Meziere <kevin@digitalmintstudios.com>
2012-06-23 01:12:38 -0700

    Final Build Before Code Camp

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CodeCamp12KinectAOADemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(12, 12);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(408, 129);
+            this.btnUp.TabIndex = 0;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(12, 147);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(408, 140);
+            this.btnDown.TabIndex = 1;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 299);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Name = "Form1";
+            this.Text = "Kinect AOA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Kinect;
+
+namespace CodeCamp12KinectAOADemo
+{
+    public partial class Form1 : Form
+    {
+        KinectSensor sensor;
+        int sensorAngle = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.sensor = KinectSensor.KinectSensors[0];
+            this.sensor.Start();
+            this.sensorAngle = this.sensor.ElevationAngle;
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                this.sensorAngle += 5;
+                this.sensor.ElevationAngle = this.sensorAngle;
+            }
+            catch
+            {
+                //Nothing
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sensorAngle -= 5;
+                this.sensor.ElevationAngle = this.sensorAngle;
+            }
+            catch
+            {
+                //Nothing
+            }
+            
+        }
+    }
+}
